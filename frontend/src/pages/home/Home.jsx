226966ba92b75a1d5920d346550d640c7470859b
Muhammed Nemathullah Khan <115334110+Nemathullahkhan@users.noreply.github.com>
2024-11-06 02:09:41 +0530

    Day-2

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,10 +4,10 @@ import MessageContainer from '../../components/messages/MessageContainer'
 
 const Home = () => {
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-grey-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
-        <SideBar/>
-        <MessageContainer/>
-    </div>
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <SideBar />
+    <MessageContainer />
+  </div>
   )
 }
 
