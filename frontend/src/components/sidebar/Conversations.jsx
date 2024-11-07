6349b5a1eef8b941811c7b3847add5f8eac04bf7
Muhammed Nemathullah Khan <115334110+Nemathullahkhan@users.noreply.github.com>
2024-11-08 02:56:45 +0530

    Day-4

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,22 @@
+import useGetConversations from "../../hooks/useGetConversations.js";
+import { getRandomEmoji } from "../../utils/emojis.js";
 import Conversation from "./Conversation.jsx";
 
 const Conversations = () => {
+	const {loading,conversations}= useGetConversations();
+	console.log("Conversations :",conversations);
 	return (
 		<div className='py-2 flex flex-col overflow-auto'>
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />
-			<Conversation />	
-			<Conversation />
+
+			{conversations.map((conversation,idx)=>(
+				<Conversation
+				key = {conversation._id}
+				conversation ={conversation}
+				emoji = {getRandomEmoji()}
+				lastIdx = {idx === conversation.length-1}
+				/>
+			))}
+			{loading ? <span className="loading loading-spinner"></span> :null}
 		</div>
 	);
 };
